9c4269bf54b0f01f1643bc444a847b6a117ff348
reszcb740 <reszcb740@Gmail.com>
2022-06-30 02:37:30 +0000

    Add controller books users

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,47 @@
+class BooksController < ApplicationController
+  def new
+    @book = Book.new
+  end
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user_id
+    @books = Book.all
+    @user = current_user
+    @book.save
+    redirect_to book_path(@book.id)
+  end
+  def index
+    @books = Book.all
+    @book = Book.new
+    @user = current_user
+  end
+
+  def show
+    @book_new = Book.new
+    @book = Book.find(params[:id])
+    @user = current_user
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+    @book.user = current_user
+    redirect_to books_path
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book.id)
+  end
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+end
