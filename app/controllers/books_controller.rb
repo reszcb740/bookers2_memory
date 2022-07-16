504f31189c70de7ea6d2494fc00b5e1a0fb8aa00
reszcb740 <reszcb740@Gmail.com>
2022-07-16 05:45:49 +0000

    Add errors books users

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,14 +1,19 @@
 class BooksController < ApplicationController
+  before_action :authenticate_user!
+   before_action :ensure_correct_user, only: [:edit, :update, :destroy]
   def new
     @book = Book.new
   end
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @books = Book.all
-    @user = current_user
-    @book.save
-    redirect_to book_path(@book.id)
+    if @book.save
+     redirect_to book_path(@book.id), notice: "You have created book successfully."
+    else
+     @books = Book.all
+     @user = current_user
+     render :index
+    end
   end
   def index
     @books = Book.all
@@ -24,14 +29,20 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
-    @book.user = current_user
-    redirect_to books_path
+    if @book.user = current_user
+      render :edit
+    else
+     redirect_to books_path
+    end
   end
 
   def update
     @book = Book.find(params[:id])
-    @book.update(book_params)
-    redirect_to book_path(@book.id)
+    if @book.update(book_params)
+     redirect_to book_path(@book.id), notice: "You have updated book successfully."
+    else
+      render :edit
+    end
   end
   def destroy
     book = Book.find(params[:id])
@@ -43,5 +54,12 @@ class BooksController < ApplicationController
 
   def book_params
     params.require(:book).permit(:title, :body)
+  end
+
+  def ensure_correct_user
+    @book = Book.find(params[:id])
+    unless @book.user == current_user
+      redirect_to books_path
+    end
   end
 end
