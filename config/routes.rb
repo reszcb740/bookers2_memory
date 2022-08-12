49ba67bc68045043cd723f3e274f165083feb92a
reszcb740 <reszcb740@Gmail.com>
2022-08-12 05:48:18 +0000

    Add controller favorite

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,12 @@ Rails.application.routes.draw do
   get "home/about" => "homes#about", as: "about"
   resources :books do
     resources :book_comments, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
   end
   resources :users, only: [:index, :show, :edit, :update] do
     resource :relationships, only: [:create, :destroy]
+    get "relationships/followings" => "relationships#followings", as: "followings"
+    get "relationships/followers" => "relationships#followers", as: "followers"
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
